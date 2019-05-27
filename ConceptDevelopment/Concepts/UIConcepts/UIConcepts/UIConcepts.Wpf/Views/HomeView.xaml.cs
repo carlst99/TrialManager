4834adc05067b86541bcd36eceed8be627e0214c
Carl Stephens <carlst99@outlook.co.nz>
2019-05-27 17:08:41 +1200

    Replace UIConcepts app with MvvmCross projects

---
+++ ConceptDevelopment/Concepts/UIConcepts/UIConcepts/UIConcepts.Wpf/Views/HomeView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+
+namespace UIConcepts.Wpf.Views
+{
+    public partial class HomeView : MvxWpfView
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
