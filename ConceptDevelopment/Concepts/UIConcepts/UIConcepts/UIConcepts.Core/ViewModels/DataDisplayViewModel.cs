1e5e0bde41a173f69e8f3719ed541d10cdcf0da2
Carl Stephens <carlst99@outlook.co.nz>
2019-06-05 10:01:04 +1200

    Updates to navigation

---
+++ ConceptDevelopment/Concepts/UIConcepts/UIConcepts/UIConcepts.Core/ViewModels/DataDisplayViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.Navigation;
+
+namespace UIConcepts.Core.ViewModels
+{
+    public class DataDisplayViewModel : Base.ViewModelBase
+    {
+        public DataDisplayViewModel(IMvxNavigationService navigationService)
+            : base(navigationService)
+        {
+        }
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+    }
+}
--- ConceptDevelopment/Concepts/UIConcepts/UIConcepts/UIConcepts.Core/ViewModels/HomeViewModel.cs
+++ ConceptDevelopment/Concepts/UIConcepts/UIConcepts/UIConcepts.Core/ViewModels/HomeViewModel.cs
@@ -1,12 +1,57 @@
-﻿using MvvmCross.Navigation;
+﻿using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCrossExtensions.Wpf.Presenters.MasterDetail;
+using System;
+using System.Collections.Generic;
 
 namespace UIConcepts.Core.ViewModels
 {
-    public class HomeViewModel : Base.ViewModelBase
+    public class HomeViewModel : Base.ViewModelBase, IMasterPresentationViewModel
     {
         public HomeViewModel(IMvxNavigationService navigationService)
             : base(navigationService)
         {
+        }
+
+        #region Fields
+
+        private bool _drawerStatus;
+        private object _detailView;
+
+        private readonly Dictionary<string, Type> _keyViewModelValuePairs = new Dictionary<string, Type>()
+        {
+            { "View/Edit Data", typeof(DataDisplayViewModel) }
+        };
+
+        #endregion
+
+        #region Commands
+
+        public IMvxCommand OnNavigateRequestedCommand => new MvxCommand(OnNavigateRequested);
+
+        #endregion
+
+        #region Properties
+
+        public bool DrawerStatus
+        {
+            get => _drawerStatus;
+            set => SetProperty(ref _drawerStatus, value);
+        }
+
+        public object DetailView
+        {
+            get => _detailView;
+            set => SetProperty(ref _detailView, value);
+        }
+
+        public Dictionary<string, Type>.KeyCollection AvailableNavigationPages => _keyViewModelValuePairs.Keys;
+
+        #endregion
+
+        private void OnNavigateRequested()
+        {
+            throw new NotImplementedException();
         }
     }
 }
