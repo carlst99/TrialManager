4b3eb088c29d047f618ef7417f792924ddfff6fd
Carl Stephens <carlst99@outlook.co.nz>
2019-06-28 17:44:33 +1200

    Some work on TimeSpanSelector

--- ConceptDevelopment/Concepts/UIConcepts/UIConcepts/UIConcepts.Wpf/Controls/TimeSpanSelector.xaml.cs
+++ ConceptDevelopment/Concepts/UIConcepts/UIConcepts/UIConcepts.Wpf/Controls/TimeSpanSelector.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace UIConcepts.Wpf
+namespace UIConcepts.Wpf.Controls
 {
     /// <summary>
     /// Interaction logic for TimeSpanSelector.xaml
@@ -45,25 +33,7 @@ namespace UIConcepts.Wpf
         public TimeSpanSelector()
         {
             InitializeComponent();
-        }
-    }
-
-    public class IntToStringConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is int intValue)
-                return value.ToString();
-            else
-                throw new ArgumentException("Value must be of type int");
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is string stringValue)
-                return int.Parse(stringValue);
-            else
-                throw new ArgumentException("Value must be of type string");
+            DataContext = this;
         }
     }
 }
---
+++ ConceptDevelopment/Concepts/UIConcepts/UIConcepts/UIConcepts.Wpf/Controls/TimeSpanToStringValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UIConcepts.Wpf.Controls
+{
+    public class TimeSpanToStringValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan time)
+                return time.ToString(@"hh\:mm");
+            else
+                throw new ArgumentException("Value must be of type " + typeof(TimeSpan));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string time)
+                return TimeSpan.Parse(time);
+            else
+                throw new ArgumentException("Value must be of type " + typeof(string));
+        }
+    }
+}
