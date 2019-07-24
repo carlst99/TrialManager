21cf9680ff9cd74be3587283db079519420383f8
Carl Stephens <carlst99@outlook.co.nz>
2019-07-25 10:35:49 +1200

    Update title string, add decoration in HomeView

--- TrialManager.Core/Resources/AppStrings.Designer.cs
+++ TrialManager.Core/Resources/AppStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace TrialManager.Core.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class AppStrings {
@@ -61,7 +61,7 @@ namespace TrialManager.Core.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to TrialManager UI Concepts.
+        ///   Looks up a localized string similar to TrialManager.
         /// </summary>
         public static string App_Title {
             get {
