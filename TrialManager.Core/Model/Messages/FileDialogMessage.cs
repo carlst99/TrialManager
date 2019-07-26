8d6453f8da56ae861feb8745c19c6f0585544716
Carl Stephens <carlst99@outlook.co.nz>
2019-07-26 18:10:43 +1200

    Add FileDialogMessage

---
+++ TrialManager.Core/Model/Messages/FileDialogMessage.cs
@@ -0,0 +1,17 @@
+﻿using IntraMessaging;
+using System;
+
+namespace TrialManager.Core.Model.Messages
+{
+    public class FileDialogMessage : Message
+    {
+        public enum DialogType
+        {
+            File, Folder
+        }
+
+        public DialogType Type { get; set; }
+        public string Title { get; set; }
+        public Action<string> Callback { get; set; }
+    }
+}
