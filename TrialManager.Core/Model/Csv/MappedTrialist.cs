36bd585caf067b1d7f1b6dc016f602ef0219fcab
Carl Stephens <carlst99@outlook.co.nz>
2019-08-09 16:57:14 +1200

    Start filling out mapping, add another dev image and rename others

---
+++ TrialManager.Core/Model/Csv/MappedTrialist.cs
@@ -0,0 +1,21 @@
+﻿using TrialManager.Core.Model.TrialistDb;
+
+namespace TrialManager.Core.Model.Csv
+{
+    public class MappedTrialist
+    {
+        public string FullName;
+        public EntityStatus Status;
+        public string Address;
+        public string PhoneNumber;
+
+        public Trialist ToTrialist()
+        {
+            Trialist trialist = new Trialist();
+
+            string[] nameComponents = FullName.Split(' ');
+
+            return trialist;
+        }
+    }
+}
---
+++ TrialManager.Core/Model/Csv/TrialistMapping.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TrialManager.Core.Model.TrialistDb;
+
+namespace TrialManager.Core.Model.Csv
+{
+    public class TrialistMapping : ClassMap<MappedTrialist>
+    {
+        public TrialistMapping()
+        {
+            Map(m => m.FullName).Index(1).Name("Full Name");
+            Map(m => m.Status).Index(2).Name("Status");
+            Map(m => m.Address).Index(3).Name("Address");
+        }
+    }
+}
