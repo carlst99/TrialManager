c172ce429f7b0f59966fa370deedbd1108091f23
Carl Stephens <carlst99@outlook.co.nz>
2019-09-06 18:20:34 +1200

    Order by descending number of dogs to help with close runs at bottom of draw

--- TrialManager.Core/Services/DrawCreationService.cs
+++ TrialManager.Core/Services/DrawCreationService.cs
@@ -37,69 +37,11 @@ namespace TrialManager.Core.Services
         {
             Realm realm = RealmHelpers.GetRealmInstance();
             _locationService.TryResolve(address, out ILocation location);
-            //SortedDictionary<DateTimeOffset, List<Trialist>> days = new SortedDictionary<DateTimeOffset, List<Trialist>>();
-            //List<Trialist> noPreferredDay = new List<Trialist>();
 
-            // Sort all trialists according to preferred day
-            //foreach (Trialist element in realm.All<Trialist>())
-            //{
-            //    if (element.PreferredDay != null && !days.ContainsKey(element.PreferredDay))
-            //        days.Add(element.PreferredDay, new List<Trialist>());
-
-            //    if (element.PreferredDay != null)
-            //    {
-            //        if (days[element.PreferredDay].Count() < maxRunsPerDay)
-            //            days[element.PreferredDay].Add(element);
-            //        else
-            //            noPreferredDay.Add(element);
-            //    } else
-            //    {
-            //        noPreferredDay.Add(element);
-            //    }
-            //}
-
-            //// Sort trialists who did not specify a preferred day
-            //foreach (Trialist element in noPreferredDay)
-            //{
-            //    bool wasPlaced = false;
-
-            //    foreach (var pair in days)
-            //    {
-            //        if (pair.Value.Count() < maxRunsPerDay)
-            //        {
-            //            pair.Value.Add(element);
-            //            wasPlaced = true;
-            //            break;
-            //        }
-            //    }
-
-            //    // If all other days were full we need to create a new day to place the trialist
-            //    if (!wasPlaced)
-            //    {
-            //        DateTimeOffset key = days.Keys.Max().AddDays(1);
-            //        days.Add(key, new List<Trialist>());
-            //        days[key].Add(element);
-            //    }
-            //}
-
-            // Sort within days by distance to trial grounds
-            //foreach (DateTimeOffset key in days.Keys)
-            //    days[key] = SortByDistance(days[key], location.Location);
-
-            //foreach (var pair in days)
-            //{
-            //    foreach (Trialist element in pair.Value)
-            //    {
-            //        foreach (Dog dog in element.Dogs)
-            //            yield return new TrialistDrawEntry(element, dog, count++, pair.Key);
-            //    }
-            //}
-
-            IEnumerable<Trialist> trialists;
+            IEnumerable<Trialist> trialists = realm.All<Trialist>();
+            trialists = trialists.ToList().OrderByDescending(t => t.Dogs.Count);
             if (location != null)
-                trialists = SortByDistance(realm.All<Trialist>(), location.Location);
-            else
-                trialists = realm.All<Trialist>();
+                trialists = SortByDistance(trialists, location.Location);
 
             foreach (TrialistDrawEntry value in SortGeneric(trialists, realm, maxRunsPerDay, startDay))
                 yield return value;
