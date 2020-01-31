2fba4195cee17367de51883b85ec203d1c6e8354
Carl Stephens <carlst99@outlook.co.nz>
2020-01-31 13:17:31 +1300

    Remove Realm and MessagePack instances from Dog

--- TrialManager/Model/TrialistDb/Dog.cs
+++ TrialManager/Model/TrialistDb/Dog.cs
@@ -1,41 +1,24 @@
-﻿using MessagePack;
-using Realms;
-
-namespace TrialManager.Model.TrialistDb
+﻿namespace TrialManager.Model.TrialistDb
 {
-    [MessagePackObject]
-    public class Dog : RealmObject, IContextItem
+    public class Dog : IContextItem
     {
         public static Dog Default => new Dog
         {
-            Id = RealmHelpers.GetNextId<Dog>(),
             Name = "Dog",
             Status = EntityStatus.Maiden
         };
 
-        private int StatusRaw { get; set; }
-
         #region Properties
-
-        [PrimaryKey]
-        public int Id { get; set; }
 
         /// <summary>
         /// Gets or sets the name of the dog
         /// </summary>
-        [Key(1)]
-        [Required]
         public string Name { get; set; }
 
         /// <summary>
         /// Gets or sets the status of the dog
         /// </summary>
-        [Key(2)]
-        public EntityStatus Status
-        {
-            get => (EntityStatus)StatusRaw;
-            set => StatusRaw = (int)value;
-        }
+        public EntityStatus Status { get; set; }
 
         #endregion
 
@@ -47,7 +30,6 @@ namespace TrialManager.Model.TrialistDb
         {
             Name = name;
             Status = status;
-            Id = RealmHelpers.GetNextId<Dog>();
         }
 
         #endregion
@@ -62,27 +44,16 @@ namespace TrialManager.Model.TrialistDb
         public override bool Equals(object obj)
         {
             return obj is Dog dog
-                && dog.Id.Equals(Id);
+                && Equals(dog);
         }
 
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hash = 13;
-                return (hash * 7) + Id.GetHashCode();
-            }
-        }
-
-        #endregion
-
-        public bool ContentEquals(Dog dog)
+        public bool Equals(Dog dog)
         {
             return dog.Name.Equals(Name)
                 && dog.Status.Equals(Status);
         }
 
-        public int GetContentHashCode()
+        public override int GetHashCode()
         {
             unchecked
             {
@@ -91,5 +62,7 @@ namespace TrialManager.Model.TrialistDb
                 return (hash * 7) + Status.GetHashCode();
             }
         }
+
+        #endregion
     }
 }
--- TrialManager/Model/TrialistDb/IContextItem.cs
+++ TrialManager/Model/TrialistDb/IContextItem.cs
@@ -2,7 +2,6 @@
 {
     public interface IContextItem
     {
-        int Id { get; set; }
         EntityStatus Status { get; set; }
         string Name { get; set; }
     }
