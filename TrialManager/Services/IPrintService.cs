1b680129625e08ba292f8da964d9bc867cbfed16
Carl Stephens <carlst99@outlook.co.nz>
2020-03-11 15:01:33 +1300

    Add print service scaffolding

---
+++ TrialManager/Services/IPrintService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TrialManager.Model;
+
+namespace TrialManager.Services
+{
+    public interface IPrintService
+    {
+        bool Print(IEnumerable<TrialistDrawEntry> drawEntries);
+    }
+}
---
+++ TrialManager/Services/PrintService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrialManager.Model;
+
+namespace TrialManager.Services
+{
+    public class PrintService : IPrintService
+    {
+        public bool Print(IEnumerable<TrialistDrawEntry> drawEntries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
