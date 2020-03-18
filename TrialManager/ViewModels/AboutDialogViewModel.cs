1c182468b92c401b5161fc218614f60daf97fc1c
Carl Stephens <carlst99@outlook.co.nz>
2020-03-18 19:09:59 +1300

    Add diagnostics toggle to AboutDialog

--- TrialManager/ViewModels/AboutDialogViewModel.cs
+++ TrialManager/ViewModels/AboutDialogViewModel.cs
@@ -1,17 +1,35 @@
-﻿using Stylet;
+﻿using Realms;
+using Stylet;
 using System.Reflection;
+using TrialManager.Model;
 using TrialManager.Services;
+using TrialManager.Utils;
 using TrialManager.ViewModels.Base;
 
 namespace TrialManager.ViewModels
 {
     public class AboutDialogViewModel : ViewModelBase
     {
+        private readonly Realm _realmInstance;
+        private readonly Preferences _preferences;
+
         public string Version => Assembly.GetEntryAssembly().GetName().Version.ToString(3);
+
+        public bool IsDiagnosticsEnabled
+        {
+            get => _preferences.IsDiagnosticsEnabled;
+            set
+            {
+                _realmInstance.Write(() => _preferences.IsDiagnosticsEnabled = value);
+                NotifyOfPropertyChange(nameof(IsDiagnosticsEnabled));
+            }
+        }
 
         public AboutDialogViewModel(IEventAggregator eventAggregator, INavigationService navigationService)
             : base (eventAggregator, navigationService)
         {
+            _realmInstance = RealmHelpers.GetRealmInstance();
+            _preferences = RealmHelpers.GetUserPreferences(_realmInstance);
         }
     }
 }
